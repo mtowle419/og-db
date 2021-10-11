dffac8d96aa7b8ab3f6f2125f348f96ecb61b39f
TL Michael <mtowle419@gmail.com>
2021-10-11 18:23:05 -0400

    init

---
+++ lib/og/db.rb
@@ -0,0 +1,8 @@
+require "og/db/version"
+
+module Og
+  module Db
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/og/db/version.rb
@@ -0,0 +1,5 @@
+module Og
+  module Db
+    VERSION = "0.1.0"
+  end
+end
